7f292a45607c5b36073765690a2076565c2f7e4d
ABCLLLLLLLL <2731132309@qq.com>
2023-09-21 16:12:43 +0800

    9.21创建了csutomProperties包，其中包含三个类，创建了SQL包，其中包含了四个类，并完成第四章作业

---
+++ src/main/java/cn/lzy/thymeleaf/MyLocalResovel.java
@@ -0,0 +1,38 @@
+package cn.lzy.thymeleaf;
+
+import com.sun.xml.bind.v2.schemagen.xmlschema.LocalAttribute;
+import io.netty.util.internal.StringUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+@Configuration
+public class MyLocalResovel implements LocaleResolver {
+    @Override
+    public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+        String l = httpServletRequest.getParameter("l");
+        String header = httpServletRequest.getHeader("Accept-Language");
+        Locale locale=null;
+        if (!StringUtil.isNullOrEmpty(l)){
+            String[] split=l.split("_");
+            locale=new Locale(split[0],split[1]);
+        }else {
+            String[] splits=header.split(",");
+            String[] split=splits[0].split("-");
+            locale=new Locale(split[0],split[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocalResovel();
+    }
+}
