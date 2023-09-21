fdc2d15e8cc53ba777eab14b387434dbb8103d70
ABCLLLLLLLL <2731132309@qq.com>
2023-09-21 17:44:26 +0800

    9.21更正类MyInterceptor

--- src/main/java/cn/lzy/Web/mvc/MyInterceptor.java
+++ src/main/java/cn/lzy/Web/mvc/MyInterceptor.java
@@ -14,19 +14,23 @@ import java.util.Calendar;
 public class MyInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
         Object loginUser = request.getSession().getAttribute("loginUser");
-        if(uri.startsWith("/admin")||(uri.startsWith("/shang") && null == loginUser)) {
+        if (uri.startsWith("/admin") || (uri.startsWith("/shang") && null == loginUser)) {
             response.sendRedirect("/toLoginPage1");
             return false;
-    }
+        }
         return true;
-}
-@Override
-public void postHandle(HttpServletRequest request, HttpServletResponse response,
-                       Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-    request.setAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
     }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        request.setAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+    }
+
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse
             response, Object handler, @Nullable Exception ex) throws Exception {
     }
+}
