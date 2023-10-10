77a6b91187acc24800664d2163c63e318ac77891
ABCLLLLLLLL <2731132309@qq.com>
2023-10-10 16:20:08 +0800

    SpringBootMVC拓展功能实现

--- src/main/java/cn/lzy/Web/mvc/MYyMVCconfig.java
+++ src/main/java/cn/lzy/Web/mvc/MYyMVCconfig.java
@@ -1,6 +1,8 @@
 package cn.lzy.Web.mvc;
 //
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,4 +13,15 @@ public class MYyMVCconfig implements WebMvcConfigurer {
         registry.addViewController("/toLoginPage").setViewName("login");
         registry.addViewController("/login.html").setViewName("login");
     }
+    @Autowired
+    private MyInterceptor myInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor)
+                .addPathPatterns("/**") //拦截所有路径请求
+                .excludePathPatterns("/login.html") //放行/ login.html 路径请求
+                .excludePathPatterns("/toLoginPage");
+    }
+
 }
--- src/main/java/cn/lzy/Web/mvc/MyInterceptor.java
+++ src/main/java/cn/lzy/Web/mvc/MyInterceptor.java
@@ -16,8 +16,8 @@ public class MyInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String uri = request.getRequestURI();
         Object loginUser = request.getSession().getAttribute("loginUser");
-        if (uri.startsWith("/admin") || (uri.startsWith("/shang") && null == loginUser)) {
-            response.sendRedirect("/toLoginPage1");
+        if (uri.startsWith("/admin")  && null == loginUser) {
+            response.sendRedirect("/toLoginPage");
             return false;
         }
         return true;
